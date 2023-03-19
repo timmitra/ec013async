6e952a8a961337e16322cf7c94ac797fcd473ca7
Tim Mitra <tim@it-guy.com>
2023-03-19 16:19:00 -0400

    handling error

--- Chapter01/00/WhosNext/Controllers/EntryVendors/ErrorEntryVendor.swift
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/ErrorEntryVendor.swift
@@ -1,12 +1,20 @@
 struct ErrorEntryVendor  {
   func entry(for count: Int) -> Entry {
-    let imageName = imageName(for: count)
-    return Entry(imageName: imageName)
+    do {
+      let imageName = try imageName(for: count)
+      return Entry(imageName: imageName)
+    } catch {
+      print(error.localizedDescription)
+      return errorEntry()
+    }
   }
 }
 
 extension ErrorEntryVendor {
-  private func imageName(for int: Int) -> String {
+  private func imageName(for int: Int) throws -> String {
+    if int.isMultiple(of: 5) {
+      throw MultipleOfFiveError(number: int)
+    }
     let number = int % 51
     return "\(number).circle"
   }
