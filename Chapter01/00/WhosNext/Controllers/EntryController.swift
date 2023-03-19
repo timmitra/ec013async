3514da4a6f48a29d8eb33f1c1ad6e273e80a1091
Tim Mitra <tim@it-guy.com>
2023-03-19 15:22:16 -0400

    “Create an array of type [Entry] named entries.”

--- Chapter01/00/WhosNext/Controllers/EntryController.swift
+++ Chapter01/00/WhosNext/Controllers/EntryController.swift
@@ -1,9 +1,14 @@
 class EntryController {
   private var count = 0
+  private let vendor = PlainEntryVendor()
+  private(set) var entries: [Entry] = []
 }
 
 extension EntryController {
   func next() {
     count += 1
+    let entriesCopy = entries
+    let newEntry = vendor.entry(for: count)
+    entries = entriesCopy + [newEntry]
   }
 }
