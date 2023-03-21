f3f39053ba4b77f3c8f0b06260d11c3683d42db6
Tim Mitra <tim@it-guy.com>
2023-03-20 23:31:12 -0400

    some tests but not super helpful for multiples of 5

--- Chapter01/00/WhosNextTests/WhosNextTests.swift
+++ Chapter01/00/WhosNextTests/WhosNextTests.swift
@@ -6,8 +6,24 @@
 //
 
 import XCTest
+@testable import WhosNext
 
 final class WhosNextTests: XCTestCase {
+  let vendor = AsyncEntryVendor()
+  let entry3 = Entry(imageName: "3.circle")
 
-
+  func test_entryCreation() async throws {
+    let result = await vendor.entry(for: 3)
+    XCTAssertEqual(result, entry3)
+  }
+  
+  func test_errorEntryCreation() async {
+    let result = await vendor.entry(for: 5)
+    XCTAssertEqual(result, errorEntry())
+  }
+  
+  func test_imageNameCreatesEntry() async throws {
+    let result = try await vendor.imageName(for: 3)
+    XCTAssertEqual(result, entry3.imageName)
+  }
 }
